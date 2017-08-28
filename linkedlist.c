b8166d991f48299268850c7f901b4973cf36a4f9
Evan Harmon <evan.p.harmon@gmail.com>
2017-08-28 13:19:40 -0700

    linkedlist insert

--- linkedlist.c
+++ linkedlist.c
@@ -3,43 +3,74 @@
 /* #include <criterion/criterion.h> */
 /* #include <criterion/logging.h> */
 
-/* #define NULL 0 */
-
-typedef struct ListNode {
+struct ListNode {
 	int data;
 	struct ListNode *next;
-} node_t;
+};
 
-int ListLength(struct ListNode *head)
-{
+int ListLength(struct ListNode *head) {
 	struct ListNode *current = head;
 	int count = 0;
 
 	while (current != NULL) {
-		printf("%d\n", current->data);
+		printf("List Length %d\n", current->data);
 		count++;
 		current = current->next;
 	}
 	return count;
 }
 
-int main(int argc, char const* argv[])
-{
+void InsertLinkedList(struct ListNode **head, int data, int position) {
+	printf("Insert \n");
+	int k = 1;
+	struct ListNode *p, *q, *newNode;
+	newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
+	if (!newNode) {
+		printf("Memory Error\n");
+		return;
+	}
+
+	newNode->data = data;
+	p = *head;
+	// Insert at Beginning
+	if (position == 1) {
+		newNode->next = p;
+		*head = newNode;
+	}
+	else {
+		// Traverse List until position where to insert
+		while ((p != NULL) && (k < position)) {
+			k++;
+			q = p;
+			p = p->next;
+		}
+		q->next = newNode;
+		newNode->next = p;
+	}
+}
+
+int main(int argc, char const* argv[]) {
 	// local variable for function
-	node_t * head = NULL;
-	head = malloc(sizeof(node_t));
+	struct ListNode *head = NULL;
+	head = malloc(sizeof(struct ListNode));
 	if (head == NULL) {
 		return 1;
 	}
 
 	head->data = 1;
-	head->next = malloc(sizeof(node_t));
+	head->next = malloc(sizeof(struct ListNode));
 	if (head->next == NULL) {
 		return 1;
 	}
 	head->next->data = 2;
-	head->next->next = NULL;
+	head->next->next = malloc(sizeof(struct ListNode));
+
+	head->next->next->data = 3;
+	head->next->next->next = NULL;
 
 	ListLength(head);
+	InsertLinkedList(&head, 5, 1);
+	ListLength(head);
+
 	return 0;
 }
