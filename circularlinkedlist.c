657b00d9b0eefa985c319181f20702c239cf5439
Evan Harmon <evan.p.harmon@gmail.com>
2017-08-30 17:24:24 -0700

    feat: add functions to circularlinkedlist

--- circularlinkedlist.c
+++ circularlinkedlist.c
@@ -35,6 +35,88 @@ void PrintCircularListData(struct CLLNode *head) {
 	} while (current != head); // NULL would cause infinite loop
 }
 
+void InsertAtBeginInCLL(struct CLLNode **head, int data) {
+	struct CLLNode *current = *head;
+	struct CLLNode *newNode = malloc(sizeof(struct CLLNode));
+	if (!newNode) {
+		printf("Memory Error \n");
+		return;
+	}
+	newNode->data = data;
+
+	while (current->next != *head) {
+		current = current->next;
+	}
+	newNode->next = newNode;
+	if (*head == NULL) {
+		*head = newNode;
+	} else {
+		newNode->next = *head;
+		current->next = newNode;
+		*head = newNode;
+	}
+
+	return;
+}
+
+void InsertAtEndInCLL(struct CLLNode **head, int data) {
+	struct CLLNode *current = *head;
+	struct CLLNode *newNode = malloc(sizeof(struct CLLNode));
+	if (!newNode) {
+		printf("Memory Error \n");
+		return;
+	}
+
+	newNode->data = data;
+	while (current->next != *head) {
+		current = current->next;
+	}
+
+	newNode->next = newNode;
+
+	if (*head == NULL) {
+		*head = newNode;
+	} else {
+		newNode->next = *head;
+		current->next = newNode;
+	}
+}
+
+void DeleteLastNodeFromCLL(struct CLLNode **head) {
+	struct CLLNode *temp = *head, *current = *head;
+	if (*head == NULL) {
+		printf("List Empty \n");
+		return;
+	}
+	while (current->next != *head) {
+		temp = current;
+		current = current->next;
+	}
+	temp->next = current->next;
+	free(current);
+
+	return;
+}
+
+void DeleteFrontNodeFromCLL(struct CLLNode **head) {
+	struct CLLNode *temp = *head;
+	struct CLLNode *current = *head;
+	if (*head == NULL) {
+		printf("List Empty\n");
+		return;
+	}
+
+	while (current->next != *head) {
+		current = current->next;
+	}
+
+	current->next = (*head)->next;
+	*head = (*head)->next;
+	free(temp);
+
+	return;
+}
+
 int main(int argc, char const* argv[])
 {
 	// Keep Track Of Head since circular list
@@ -49,6 +131,13 @@ int main(int argc, char const* argv[])
 	head->next = head;
 
 	CircularListLength(head);
+
+	// Item 2
+	InsertAtEndInCLL(&head, 2);
+	InsertAtBeginInCLL(&head, 3);
+
+	DeleteFrontNodeFromCLL(&head);
+	DeleteLastNodeFromCLL(&head);
 	PrintCircularListData(head);
 
 	return 0;
