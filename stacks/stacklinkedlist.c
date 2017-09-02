c7ee7e7e25af1fb76611e35213e3dfde8115c094
Evan Harmon <evan.p.harmon@gmail.com>
2017-09-02 16:57:55 -0700

    bug: fix delete stack function

--- stacks/stacklinkedlist.c
+++ stacks/stacklinkedlist.c
@@ -8,72 +8,93 @@ struct Stack {
 	struct Stack *next;
 };
 
-struct Stack *CreateStack() {
-	return NULL;
+struct Stack *newStack(int val) {
+	struct Stack *s = malloc(sizeof(struct Stack));
+	if (!s) {
+		return NULL;
+	}
+	s->data = val;
+	s->next = NULL;
+	return s;
 };
 
 void Push(struct Stack **top, int data) {
-	struct Stack *temp;
-	temp = malloc(sizeof(struct Stack));
-	if (!temp) {
+	struct Stack *s = newStack(data);
+	if (!s) {
 		return;
 	}
-	temp->data = data;
-	temp->next = *top;
-	*top = temp;
+	s->next = *top;
+	*top = s;
 };
 
 int IsEmptyStack(struct Stack *top) {
-	printf("top %p, top->next %p\n", top, top->next);
-	return (top == NULL);
+	if (!top) {
+		printf("Empty top %p\n", top);
+	} else {
+		printf("Empty top %p, top->next %p\n", top, top->next);
+	}
+	// is NULL?
+	return !top;
 };
 
 int Pop(struct Stack **top) {
 	int data;
-	struct Stack *temp;
 	if (IsEmptyStack(*top)) {
 		return INT_MIN;
 	}
-	temp = *top;
-	*top = temp->next;
+	struct Stack *temp = *top;
+	*top = (*top)->next;
 	data = temp->data;
 	free(temp);
 	return data;
 };
 
-int Top(struct Stack *top) {
+int Peek(struct Stack *top) {
 	if (IsEmptyStack(top)) {
 		return INT_MIN;
 	}
-	return top->next->data;
+	return top->data;
 };
 
-void DeleteStack(struct Stack **top) {
-	struct Stack *temp, *p;
-	p = *top;
-	while (p->next) {
-		temp = p->next;
-		p->next = temp->next;
+void DeleteStack(struct Stack **list) {
+	// access pointer of the parameter from function call
+	struct Stack *top = *list;
+	/* printf("Before **list %p, *list %p, list next %p\n", (* list), list, (* list)->next); */
+	while (top->next) {
+		// temp store of struct to free mem later
+		struct Stack *temp = top->next;
+		/* printf("Pre del temp %p, top %p, top next %p, top next next %p\n", temp, top, top->next, top->next->next); */
+		// move to next pointer
+		top->next = top->next->next;
+		/* printf("Post del temp %p, temp next %p, top %p, top next %p\n", temp, temp->next, top, top->next); */
 		free(temp);
+		// defensive set to null
+		temp = NULL;
 	}
-	free(p);
+	/* printf("After **list %p, *list %p, top %p, top next %p\n", (* list), list, top, top->next); */
+	// free list
+	free(top);
+	top = NULL;
+	// set list value to null
+	*list = NULL;
+	/* printf("Free delete **list %p, *list %p, top %p\n", (* list), list, top); */
 };
 
 int main(int argc, char const* argv[])
 {
-	struct Stack *S = CreateStack();
+	struct Stack *S = NULL;
 	Push(&S, 1);
 	Push(&S, 2);
-	int t, p, e;
-	t = -1;
-	p = -1;
-	e = -1;
+	Push(&S, 3);
 
-	t = Top(S);
-	p = Pop(&S);
+	/* printf("2 Push Top %d\n", Peek(S)); */
+	Pop(&S);
+	/* printf("1 Pop Top %d\n", Peek(S)); */
+	/* Pop(&S); */
 
 	DeleteStack(&S);
-	e = IsEmptyStack(S);
+	printf("Stack %p, &Stack %p\n", S, &S);
+	printf("Empty? %d\n", IsEmptyStack(S));
 
 	return 0;
 }
