5a8736e439bc33e030276fe19edef06d0fccf1e1
Evan Harmon <evan.p.harmon@gmail.com>
2017-09-08 13:20:01 -0700

    feat: BFT with Queue working correctly

--- trees/breadthfirsttraversalv2.c
+++ trees/breadthfirsttraversalv2.c
@@ -53,25 +53,26 @@ void EnQueue(struct Queue *q, struct Node *node) {
 	assert(node);
 	// create a new list
 	struct List *list = newList(node);
-	// attach new list to Queue
-	if (!q->front) {
-		q->front = list;
-	} else {
-		q->rear = list;
-	}
+	// attach new list item as last (rear next) of the Queue
+	if (q->rear) q->rear->next = list;
+	// important - update rear so it represents "last" item
+	q->rear = list;
+	/* this ties it all together, make front equal the first rear
+	this means the front->next ties in with the whole queue list of rear
+	so you can follow front->next recursively all the way
+	to the last rear in a large queue */
+	if (!q->front)
+		q->front = q->rear;
 };
 
 struct Node *DeQueue(struct Queue *q) {
 	assert(IsEmpty(q) == 0);
 	// store old list pointer
-	struct List *res = q->front;
-	q->front = q->rear;
-	if (q->rear) {
-		q->rear = q->rear->next;	
-	} else {
-		q->rear = NULL;
-	}
-	return res->node;
+	struct List *temp = q->front;
+	struct Node *node = temp->node;
+	q->front = q->front->next;	
+	free(temp);
+	return node;
 };
 
 void PrintTree(struct Node *root) {
@@ -84,23 +85,43 @@ void PrintTree(struct Node *root) {
 	return;
 };
 
-void PrintQueue(struct Queue *Q) {
+void PrintQueue(struct Queue *Q, char name[]) {
 	int cnt = 0;
 	struct List *temp = Q->front;
+	if (Q->front)
+		printf("%s, FR cnt %d, Q fr %p, d %d ", name, cnt, temp, temp->node->data);
+	if (Q->rear) {
+		printf("RR %p, d %d\n", Q->rear, Q->rear->node->data);
+	} else {
+		printf("rr null\n");
+	}
 	while (temp) {
-		printf("cnt %d, Q fr %p, d %d ", cnt, temp, temp->node->data);
-		if (Q->rear) {
-			printf("Q rr %p, d %d\n", Q->rear, Q->rear->node->data);
-		} else {
-			printf("rr null\n");
-		}
-		printf("nxt %p\n", temp->next);
+		printf("\tcnt %d, nxt %p\n", cnt, temp->next);
 		temp = temp->next;
 		cnt++;
-		printf("cnt %d\n", cnt);
 	}
 	free(temp);
 	temp = NULL;
+};
+
+void PrintLevelOrder(struct Node *root) {
+	struct Node *temp = root;
+	struct Queue *q = newQueue();
+	while (temp) {
+		printf("temp %p, %d \n", temp, temp->data);
+
+		// for each node, add left and right to force traversal of both sides of node
+		if (temp->left)
+			EnQueue(q, temp->left);
+		if (temp->right)
+			EnQueue(q, temp->right);
+
+		if (IsEmpty(q)) {
+			break;
+		} else {
+			temp = DeQueue(q);
+		}
+	}
 };
 
 int main(int argc, char *argv[])
@@ -111,33 +132,31 @@ int main(int argc, char *argv[])
 	root->left->left = newNode(4);
 	root->left->right = newNode(5);
 
-	struct Queue *q = newQueue();
-	EnQueue(q, root);
-	PrintQueue(q); // BUG: acting like a stack not a queue
-
-	EnQueue(q, root->left);
-	PrintQueue(q);
-	/* printf("1EQ fr %p, rr %p, node %p, data %d\n", q->front, q->rear, q->front->node, q->front->node->data); */
-
+	/* struct Queue *q = newQueue(); */
+	/* EnQueue(q, root); */
+	/* PrintQueue(q, "EnQueue"); // BUG: acting like a stack not a queue */
+	/* EnQueue(q, root->left); */
+	/* PrintQueue(q, "EnQueue"); // BUG: acting like a stack not a queue */
 	/* EnQueue(q, root->right); */
-	/* PrintQueue(q); */
-
+	/* PrintQueue(q, "EnQueue"); // BUG: acting like a stack not a queue */
 	/* EnQueue(q, root->left->left); */
-	/* PrintQueue(q); */
+	/* PrintQueue(q, "EnQueue"); // BUG: acting like a stack not a queue */
 	/* EnQueue(q, root->left->right); */
-	/* PrintQueue(q); */
+	/* PrintQueue(q, "EnQueue"); // BUG: acting like a stack not a queue */
 
 	/* DeQueue(q); */
-	/* printf("1DEQ fr %p, rr %p, node %p, data %d\n", q->front, q->rear, q->front->node, q->front->node->data); */
+	/* PrintQueue(q, "DeQueue"); // BUG: acting like a stack not a queue */
 	/* DeQueue(q); */
-	
+	/* PrintQueue(q, "DeQueue"); // BUG: acting like a stack not a queue */
+	/* DeQueue(q); */
+	/* PrintQueue(q, "DeQueue"); // BUG: acting like a stack not a queue */
 
-	/* printf("\t\t *** BINARY TREE ***\n"); */
-	/* PrintTree(root); */
+	printf("\t\t *** BINARY TREE ***\n");
+	PrintTree(root);
 
-	/* printf("*** Recursive Traverse (1 2 3 4 5)\n"); */
-	/* PrintLevelOrder(root); */
-	/* printf("\n"); */
+	printf("*** Recursive Traverse (1 2 3 4 5)\n");
+	PrintLevelOrder(root);
+	printf("\n");
 
 	return 0;
 }
