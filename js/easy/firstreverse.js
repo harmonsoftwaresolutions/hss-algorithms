9fbb0b753f3dd1168d440416f9cc30202f950fce
Evan Harmon <eharmon@izotope.com>
2019-11-14 08:54:11 -0500

    move js code-challenge repo files to here

---
+++ js/easy/firstreverse.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+
+// Have the function firstReverse(str) take the str parameter being passed and
+// return the string in reversed order. For example: if the input string is
+// "Hello World and Coders" then your program should return the string
+// sredoC dna dlroW olleH. 
+
+function firstReverse(str) { 
+  const chars = str.split("");
+
+  const arr = [];
+  const ln = str.length;
+  let idx = ln;
+  for(let i = 0; i < ln; i++) {
+      arr.push(chars[idx - 1]);
+      idx = idx - 1;
+  }
+  
+  const revStr = arr.join("");
+  return revStr; 
+}
+   
+const a1 = "coderbyte";
+const r1 = "etybredoc";
+const t1 = firstReverse(a1);                            
+assert.strictEqual(t1, r1, `should be ${r1}`);
+
+const a2 = "I Love Code";
+const r2 = "edoC evoL I";
+const t2 = firstReverse(a2);                            
+assert.strictEqual(t2, r2, `should be ${r2}`);
